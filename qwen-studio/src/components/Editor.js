97ccd157185feb77a0a74354ba6d7e097644a9ad
lololololoooooooooo <asimbeats07@gmail.com>
2025-08-30 12:26:13 +0530

    Update Editor.js

--- qwen-studio/src/components/Editor.js
+++ qwen-studio/src/components/Editor.js
@@ -1,1 +1,27 @@
+// src/components/Editor.js
+import Editor from '@monaco-editor/react';
 
+export default function CodeEditor({ value, onChange, language = "python" }) {
+  const handleEditorChange = (newValue) => {
+    onChange(newValue);
+  };
+
+  return (
+    <div className="flex-1 flex flex-col">
+      <Editor
+        height="100%"
+        width="100%"
+        language={language}
+        theme="vs-dark"
+        value={value}
+        onChange={handleEditorChange}
+        options={{
+          minimap: { enabled: false },
+          fontSize: 14,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+}
